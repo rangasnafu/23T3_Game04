91853fd2c7d095dd88d39ce9b2232d5f655ab2b2
rangasnafu - Home <135566054+rangasnafu@users.noreply.github.com>
2023-10-18 10:58:08 +1100

    add playercontroller asset to scene

---
+++ Assets/3rdPerson+Fly/Scripts/HayleyScripts/Shooting.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawnPoint;
+    public float shootInterval;
+    private float shootTimer;
+
+    public AudioClip shootSound;
+    //private AudioSource audioSource;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //audioSource = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateShooting();
+
+    }
+
+    private void UpdateShooting()
+    {
+        shootTimer -= Time.deltaTime;
+
+        if (shootTimer <= 0 && Input.GetKey(KeyCode.Space))
+        {
+            shootTimer = shootInterval;
+            ShootBullet();
+
+        }
+    }
+
+    private void ShootBullet()
+    {
+        //audioSource.PlayOneShot(shootSound);
+        Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
+        //instantiate means to spawn, quaternion means rotation
+    }
+}
