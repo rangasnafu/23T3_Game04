f69e4bb174008b06a7212c3e04da69f9d33ad216
rangasnafu - Home <135566054+rangasnafu@users.noreply.github.com>
2023-10-20 00:35:59 +1100

    enemy ai (doesn't work)

--- Assets/3rdPerson+Fly/Scripts/HayleyScripts/MoveForce.cs
+++ Assets/3rdPerson+Fly/Scripts/HayleyScripts/MoveForce.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class MoveForce : MonoBehaviour
 {
+    public Vector3 movementSpeed;
+    public Space space;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //move the bullet 
+        transform.Translate(movementSpeed * Time.deltaTime, space);
+
+
+    }
+
+    // Destory the bullet when it collides with the Bullet Destroyer
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Force collided with Enemy");
+            Destroy(gameObject);
+
+        }
     }
 }
